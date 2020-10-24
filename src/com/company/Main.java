4ab843a19aa23f08f8bbe661eaf35369e6bf5b83
Ivka374 <ivka@dsoft-bg.com>
2020-10-24 12:05:51 +0300

    Added the option to check your last transaction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     private static double balance = 0;
+    private static String lastTransaction;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,7 +13,8 @@ public class Main {
         while (i){
             System.out.println("Type 'Q' to exit\n" +
                     "'T' to make a transaction\n" +
-                    "or 'B' to check balance");
+                    "'B' to check balance\n" +
+                    "or 'L' for seeing your last transaction");
             switch (scanner.nextLine()){
                 case "T": case "t":
                     System.out.println("Enter value to add to account:");
@@ -25,6 +27,9 @@ public class Main {
                 case "Q": case "q":
                     i = false;
                     break;
+                case "L": case "l":
+                    System.out.println(lastTransaction);
+                    break;
                 default:
                     System.out.println("Invalid command");
                     break;
@@ -33,11 +38,23 @@ public class Main {
 
     }
 
-    public static void activity(double value){
+    private static void activity(double value){
         balance += value;
         if (value > 0) System.out.println("You have deposited " + value + " in your account");
         if (value == 0) System.out.println("Action canceled");
         if (value < 0) System.out.println("You have withdrawn " + (-value) + " from your account");
+        setLastTransaction(value);
+    }
+
+    private static void setLastTransaction(double value){
+        lastTransaction = "You ";
+        if (value < 0){
+            lastTransaction += "withdrew ";
+            lastTransaction += -value;
+        }else {
+            lastTransaction += "deposited ";
+            lastTransaction += value;
+        }
     }
 
 }
